093d64372cf2f684d11f8015e56c6822ac995438
HecticKaluha <stefanoverhoeve44@gmail.com>
2018-06-05 12:19:35 +0200

    initial commit
    set up the different services.

---
+++ contractor_products/src/main/java/controller/JaxApi.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxApi extends Application
+{
+    /*@Override
+    public Set<Class<?>> getClasses()
+    {
+        Set<Class<?>> restClass = new HashSet<>();
+        restClass.add(KweetController.class);
+        return restClass;
+    }*/
+}
